56aab8911ea967d44ca10008796933b961eeacf2
rahimovozodbek102-ai <rahimovozodbek102@gmail.com>
2026-02-06 01:55:51 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// doppi ai scripts
+function setLang(lang) {
+  const text = document.getElementById("welcomeText");
+
+  if (lang === "uz") {
+    text.innerText = "DO’PPI AI ga xush kelibsiz";
+  }
+  if (lang === "ru") {
+    text.innerText = "Добро пожаловать в DO’PPI AI";
+  }
+  if (lang === "en") {
+    text.innerText = "Welcome to DO’PPI AI";
+  }
+
+  localStorage.setItem("lang", lang);
+}
